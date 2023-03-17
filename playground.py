6692824b7eef3214e88cfb1f8da1d95514de4e7a
imjayapple <imjayapple@gmail.com>
2023-03-16 21:12:52 -0700

    Initial commit, understanding Python variables and how to compute them

---
+++ playground.py
@@ -0,0 +1,35 @@
+#integer variable
+rating_out_of_10 = 5
+
+#float
+test_score = 98.1
+
+#print out the result of this equation
+print(25 * 68 + 13 / 28)
+
+#reassign variables
+quilt_width = 8
+quilt_length = 12
+
+print(quilt_width * quilt_length)
+
+quilt_width = 8
+quilt_length = 8
+
+print(quilt_width * quilt_length)
+
+#perform exponential multiplication of variable
+
+# Calculation of squares for:
+# 6x6 quilt
+quilt_length = 6
+print(quilt_length ** 2)
+# 7x7 quilt
+quilt_length = 7
+print(quilt_length ** 2)
+# 8x8 quilt
+quilt_length = 8
+print(quilt_length ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+quilt_length = 6
+print(quilt_length ** 4)
